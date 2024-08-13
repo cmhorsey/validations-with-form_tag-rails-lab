975bbb9751789ad1ca7a22ee1a700e16ccba6710
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-08-12 20:02:32 -0400

    Add edit form and render error messages for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,28 +3,31 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
   def edit
     @post = Post.find(params[:id])
   end
 
-  # def update
-  #   @post = Post.find(params[:id])
-
-  #   @post.update(post_params)
-
-  #   redirect_to post_path(@post)
-  # end
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
 
   def update
     @post = Post.find(params[:id])
-
     if @post.update(post_params)
       redirect_to post_path(@post)
     else
       render :edit
     end
   end
-
   private
 
   def post_params
